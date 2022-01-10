56b38fbfcddf3411ab085f8a6f5a51ca37358012
SCxoneZ <stevenniuschandraa@gmail.com>
2022-01-10 20:21:29 +0700

    just adding some functions

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const container = document.querySelector('.container');
 const selectTo = document.querySelector('select.to');
 const selectFrom = document.querySelector('select.from');
-
 for(const c in countries){
   const optionTo = document.createElement('option');
   const optionFrom = document.createElement('option');
@@ -15,3 +14,20 @@ for(const c in countries){
   selectFrom.appendChild(optionFrom);
 }
 
+
+
+const exchangeUrl = (curr) => `https://v6.exchangerate-api.com/v6/75fe1a1155ffd487694cab79/latest/${curr}`;
+
+const detailUrl = (country) => `https://restcountries.com/v3.1/name/${country}`;
+
+
+async function exchange(){
+  const details = await getData('https://restcountries.com/v3.1/name/indonesia');
+  console.log(details);
+}
+
+exchange();
+
+function getData(url){
+  return fetch(url).then(res => res.json()).then(data => data).catch(err => err);
+}
