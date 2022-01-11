dfef38e8cab3501565969ec1f03cb6531b21e4b8
SCxoneZ <stevenniuschandraa@gmail.com>
2022-01-11 15:34:19 +0700

    finishing(80%)

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// https://v6.exchangerate-api.com/v6/75fe1a1155ffd487694cab79/latest/IDR
-
 const container = document.querySelector('.container');
 const selectTo = document.querySelector('select.to');
 const selectFrom = document.querySelector('select.from');
@@ -13,29 +11,30 @@ doLoop();
 swapBtn.addEventListener('click', () => {
   const toVal = selectTo.value;
   const fromVal = selectFrom.value;
-  selectTo.value = fromVal;
-  selectFrom.value = toVal;
 })
 
 btnExchange.addEventListener('click', async () => {
-  if(input.value && !isNaN(parseInt(input.value))){
-    const data = await getData(exchangeUrl(selectFrom.value));
-    result.innerHTML = `${data.conversion_rates[selectTo.value] * parseInt(input.value)} ${selectTo.value}`;
-    swapBtn.style.transform = 'rotate(180deg)';
+  if (input.value && !isNaN(parseInt(input.value))) {
+    const arrTo = selectTo.value.split(' ');
+    const arrFrom = selectFrom.value.split(' ');
+    const selectedCurrTo = arrTo[0];
+    const selectedCurrFrom = arrFrom[0];
+    arrTo.shift();
+    arrFrom.shift();
+    const selectedToCountry = arrTo.join(' ');
+    const selectedFromCountry = arrFrom.join(' ');
+    const data = await getData(exchangeUrl(selectedCurrTo, selectedCurrFrom, input.value));
+    if (data.result == 'success') {
+      const outputP = document.querySelector('.output p.details');
+      outputP.innerHTML = `Exchangin from <span class="spanFrom fw-bold">${selectedFromCountry}</span> to <span class="spanTo fw-bold">${selectedToCountry}</span>`;
+      result.innerHTML = data.conversion_result + ' ' + data.target_code;
+    }
   }
 });
 
-const exchangeUrl = (curr) => `https://v6.exchangerate-api.com/v6/75fe1a1155ffd487694cab79/latest/${curr}`;
 
+const exchangeUrl = (curr1, curr2, amount) => `https://v6.exchangerate-api.com/v6/75fe1a1155ffd487694cab79/pair/${curr2}/${curr1}/${amount}`;
 const detailUrl = (country) => `https://restcountries.com/v3.1/name/${country}`;
-
-
-async function exchange() {
-  const details = await getData('https://restcountries.com/v3.1/all');
-  // console.log(details);
-}
-
-exchange();
 
 function getData(url) {
   return fetch(url).then(res => res.json()).then(data => data).catch(err => err);
@@ -48,18 +47,16 @@ async function doLoop() {
       if (c.currencies) {
         const optionTo = document.createElement('option');
         const optionFrom = document.createElement('option');
+
         optionTo.innerHTML = c.flag + c.name.common;
-        optionTo.setAttribute('value', Object.keys(c.currencies)[0]);
-        // optionTo.dataset.symbol = c.currencies[Object.keys(c.currencies)[0]].symbol
+        optionTo.setAttribute('value', Object.keys(c.currencies)[0] + ' ' + c.name.common);
+
         optionFrom.innerHTML = c.flag + c.name.common;
-        optionFrom.setAttribute('value', Object.keys(c.currencies)[0]);
-        // optionFrom.dataset.symbol = c.currencies[Object.keys(c.currencies)[0]].symbol;
+        optionFrom.setAttribute('value', Object.keys(c.currencies)[0] + ' ' + c.name.common);
+
         selectTo.appendChild(optionTo);
         selectFrom.appendChild(optionFrom);
       }
     }
-    // if(c.currencies) console.log(Object.keys(c.currencies)[0]);
   }
-
-
 }
